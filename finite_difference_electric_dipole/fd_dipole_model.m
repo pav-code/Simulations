38b9821f86651444fd052329e38a258052c8b3b1
pav <me@pavmail.ca>
2022-09-12 16:07:05 -0400

    [fix] - removed the meta-chars

--- finite_difference_electric_dipole/fd_dipole_model.m
+++ finite_difference_electric_dipole/fd_dipole_model.m
@@ -2,13 +2,11 @@ clc
 close all; clear all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Finite Difference Solution of Laplace Equation
-
-%%    4BC3 - Modelling of Biomedical Systems
+%% Finite Difference Solution of Laplace Equation %%
+%%    4BC3 - Modelling of Biomedical Systems      %%
 %%             Pavel Gueorguiev                   %%
 %%            McMaster University                 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%           Modifiable variables                 %%
@@ -41,11 +39,11 @@ d = dR / 100;
 pQ = [0 d];
 nQ = [0 -d];
 for (i = 1:nTheta)
-  theta = i*dTheta + dThOff;
-  [x,y] = pol2cart(theta,dR);
-  r1 = ((pQ(1) - x)^2 + (pQ(2) - y)^2)^0.5;
-  r2 = ((nQ(1) - x)^2 + (nQ(2) - y)^2)^0.5;
-  V(i) = q/(4*pi*eps)*(1/r1 - 1/r2);
+    theta = i*dTheta + dThOff;
+    [x,y] = pol2cart(theta,dR);
+    r1 = ((pQ(1) - x)^2 + (pQ(2) - y)^2)^0.5;
+    r2 = ((nQ(1) - x)^2 + (nQ(2) - y)^2)^0.5;
+    V(i) = q/(4*pi*eps)*(1/r1 - 1/r2);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -58,62 +56,70 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 eqCount = 1;
 for i = 2:nR-1
-  for j = 1:nTheta
-    A = (1/(dR^2) + 1/(i*(dR^2)));
-    B = (-2/(dR^2) - 2/((i*dR)^2*(dTheta)^2));
-    C = (-1/(i*(dR^2)) + 1/(dR^2));
-    D = (1/((i*dR)^2*(dTheta)^2) + cot((j*dTheta)+dThOff)/(2*(dTheta)*(i*dR)^2));
-    E = (1/((i*dR)^2*(dTheta)^2) - cot((j*dTheta)+dThOff)/(2*(dTheta)*(i*dR)^2));
+    for j = 1:nTheta
+      A = (1/(dR^2) + 1/(i*(dR^2)));
+      B = (-2/(dR^2) - 2/((i*dR)^2*(dTheta)^2));
+      C = (-1/(i*(dR^2)) + 1/(dR^2));
+      D = (1/((i*dR)^2*(dTheta)^2) + cot((j*dTheta)+dThOff)/(2*(dTheta)*(i*dR)^2));
+      E = (1/((i*dR)^2*(dTheta)^2) - cot((j*dTheta)+dThOff)/(2*(dTheta)*(i*dR)^2));
 	
-	if (i == 2)			                        %BC 1) Dirichlet Boundary	
-      b(eqCount) = -1*C*V(j);
-	end
+	    if (i == 2)			                        %BC 1) Dirichlet Boundary	
+          b(eqCount) = -1*C*V(j);
+	    end
 	
-    if (j == 1)				                    %Case: V(#,1)->V(#,0) = V(#,N)
-      %C->(theta-2)-> 0BD ->(theta-2)-> A  *Caution* E (shift +Theta) [C 0BD EA]
-      z(eqCount,eqCount) = B;
-      z(eqCount,eqCount+1) = D;
-      z(eqCount,(eqCount-1)+nTheta) = E;
-	  if (i != 2)                               %BC 1) Dirichlet Boundary
-        z(eqCount,eqCount-nTheta) = C;
-	  end
-	  if (i != (nR-1))
-        z(eqCount,eqCount+nTheta) = A;
-      else
-        %A = (1/(dR^2) + 1/((i+1)*(dR^2)));	  
-        z(eqCount,eqCount) += A;                %BC 2) Neumann Boundary 
-      end	  
-	elseif (j == nTheta)			            %Case: V(#,N)->V(#,N+1) = V(#,1)
-	  %C->(theta-2)-> EB0 ->(theta-2)-> A *Caution* D (shift -Theta) [CD EB0 A]
-	    z(eqCount,eqCount) = B;
-	    z(eqCount,(eqCount+1)-nTheta) = D;
-	    z(eqCount,eqCount-1) = E;
-		if (i != 2)		                        %BC 1) Dirichlet Boundary
-	      z(eqCount,eqCount-nTheta) = C;
-		end
-		if (i != (nR-1))                        %BC 2) Neumann Boundary 
-          z(eqCount,eqCount+nTheta) = A;
-        else
-		  %A = (1/(dR^2) + 1/((i+1)*(dR^2)));
-		  z(eqCount,eqCount) += A;
-        end		
-	else
-	  %C->(theta-2)-> EBD ->(theta-2)-> A [C EBD A]
-	  z(eqCount,eqCount) = B;
-	  z(eqCount,eqCount+1) = D;
-	  z(eqCount,eqCount-1) = E;
-	  if (i != 2)		                        %BC 1) Dirichlet Boundary
-	    z(eqCount,eqCount-nTheta) = C;
-	  end
-	  if (i != (nR-1))
-        z(eqCount,eqCount+nTheta) = A;          %BC 2) Neumann Boundary 
-      else
-	    %A = (1/(dR^2) + 1/((i+1)*(dR^2)));
-	    z(eqCount,eqCount) += A;
-      end		
-	end
-	eqCount = eqCount + 1;
-  end
+      if (j == 1)				                    %Case: V(#,1)->V(#,0) = V(#,N)
+          %C->(theta-2)-> 0BD ->(theta-2)-> A  *Caution* E (shift +Theta) [C 0BD EA]
+          z(eqCount,eqCount) = B;
+          z(eqCount,eqCount+1) = D;
+          z(eqCount,(eqCount-1)+nTheta) = E;
+	      
+          if (i != 2)                               %BC 1) Dirichlet Boundary
+              z(eqCount,eqCount-nTheta) = C;
+	        end
+	        
+          if (i != (nR-1))
+              z(eqCount,eqCount+nTheta) = A;
+          else
+              %A = (1/(dR^2) + 1/((i+1)*(dR^2)));	  
+              z(eqCount,eqCount) += A;                %BC 2) Neumann Boundary 
+          end	  
+	    
+      elseif (j == nTheta)			            %Case: V(#,N)->V(#,N+1) = V(#,1)
+	        %C->(theta-2)-> EB0 ->(theta-2)-> A *Caution* D (shift -Theta) [CD EB0 A]
+	        z(eqCount,eqCount) = B;
+	        z(eqCount,(eqCount+1)-nTheta) = D;
+	        z(eqCount,eqCount-1) = E;
+		    
+          if (i != 2)		                        %BC 1) Dirichlet Boundary
+	            z(eqCount,eqCount-nTheta) = C;
+		      end
+		
+          if (i != (nR-1))                        %BC 2) Neumann Boundary 
+              z(eqCount,eqCount+nTheta) = A;
+          else
+		          %A = (1/(dR^2) + 1/((i+1)*(dR^2)));
+		          z(eqCount,eqCount) += A;
+          end		
+	    else
+	        %C->(theta-2)-> EBD ->(theta-2)-> A [C EBD A]
+	        z(eqCount,eqCount) = B;
+	        z(eqCount,eqCount+1) = D;
+	        z(eqCount,eqCount-1) = E;
+	  
+          if (i != 2)		                        %BC 1) Dirichlet Boundary
+	            z(eqCount,eqCount-nTheta) = C;
+	        end
+	  
+          if (i != (nR-1))
+              z(eqCount,eqCount+nTheta) = A;          %BC 2) Neumann Boundary 
+          else
+	            %A = (1/(dR^2) + 1/((i+1)*(dR^2)));
+	            z(eqCount,eqCount) += A;
+          end		
+	    end
+	    
+      eqCount = eqCount + 1;
+    end
 end
 
 Vmap = inv(z)*b;
@@ -132,24 +138,25 @@ theta = zeros((nR-1)*nTheta,1);
 r = zeros((nR-1)*nTheta,1);
 
 for i = 1:nR-1
-  for j = 1:nTheta
-    theta(count) = dTheta*count;
-	r(count) = dR*Rcount;
-    Vcontour(i,j) = Vmap(count);	
-	count=count+1;
-  end
-  Rcount=Rcount+1;
+    for j = 1:nTheta
+        theta(count) = dTheta*count;
+        r(count) = dR*Rcount;
+        Vcontour(i,j) = Vmap(count);	
+        count=count+1;
+    end
+    
+    Rcount=Rcount+1;
 end
 
 theta = theta + dThOff;
 [x, y] = pol2cart (theta, r);
 count = 1;
 for i = 1:nR-1
-  for j = 1:nTheta
-    xMap(i,j) = x(count);
-	yMap(i,j) = y(count);
-	count+=1;
-  end
+    for j = 1:nTheta
+        xMap(i,j) = x(count);
+        yMap(i,j) = y(count);
+        count+=1;
+    end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
